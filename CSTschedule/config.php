19980a017fda249acf17e85244a0337ddddb7897
Denis Turitsa <dturitsa@yahoo.ca>
2014-05-13 10:54:53 -0700

    Revert "Initial File Upload"
    This reverts commit 2ca131c182967d39b3dc7d91e9e41525c8148149.

--- CSTschedule/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_HOST', 'mysql9.000webhost.com');
-    define('DB_USER', 'a1248576_cst');
-    define('DB_PASSWORD', 'cst1234');
-    define('DB_DATABASE', 'a1248576_cst');
-    define('HOMEURL', 'index.php');
-?>
