1dd82c2741f09e35509394a427e4d6e649434f35
Austin Lott <ark.sky.dragon@gmail.com>
2014-05-19 13:22:46 -0700

    real fix for config.php

--- CSTschedule/config.php
+++ CSTschedule/config.php
@@ -3,5 +3,5 @@
     define('DB_USER', 'a1248576_cst');
     define('DB_PASSWORD', 'cst1234');
     define('DB_DATABASE', 'a1248576_cst');
-    define('HOMEURL', 'CSTSchedule.php');
+    define('HOMEURL', 'CSTSchedule.html');
 ?>
